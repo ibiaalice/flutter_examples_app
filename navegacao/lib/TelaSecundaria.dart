d061d48f91eb1f863d6670668d2276cbcc4d31f4
ibiaalice <beatrizalice00@gmail.com>
2019-12-19 09:19:59 -0300

    add umas coisas

--- navegacao/lib/TelaSecundaria.dart
+++ navegacao/lib/TelaSecundaria.dart
@@ -11,7 +11,40 @@ class TelaSecundaria extends StatefulWidget {
 }
 
 class _TelaSecundariaState extends State<TelaSecundaria> {
+  //chave global
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  DrawerController controller = DrawerController(
+    alignment: DrawerAlignment.end,
+    child: ListView(
+      children: <Widget>[
+        ListTile(
+          title: Text('Carteira'),
+          trailing: Icon(Icons.account_balance_wallet),
+          onTap: () {
+            print('apertou carteira');
+          },
+        ),
+        ListTile(
+          title: Text('Carteira'),
+          trailing: Icon(Icons.account_balance_wallet),
+          onTap: () {
+            print('apertou carteira');
+          },
+        ),
+        ListTile(
+          title: Text('Carteira'),
+          trailing: Icon(Icons.account_balance_wallet),
+          onTap: () {
+            print('apertou carteira');
+          },
+        ),
+      ],
+    ),
+  );
+
   Widget draw = Drawer(
+    key: ObjectKey('keyDraw'),
     child: ListView(
       children: <Widget>[
         UserAccountsDrawerHeader(
@@ -46,6 +79,7 @@ class _TelaSecundariaState extends State<TelaSecundaria> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(
           'Tela Secundária',
@@ -60,7 +94,50 @@ class _TelaSecundariaState extends State<TelaSecundaria> {
           ],
         ),
       ),
-      drawer: this.draw,
+      //drawer: this.draw,
+      drawer: Drawer(
+        key: ObjectKey('keyDraw'),
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Beatriz Alice'),
+              accountEmail: Text('ibiaalice@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Text(
+                  'foto ',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Carteira'),
+              trailing: Icon(Icons.account_balance_wallet),
+              onTap: () {
+                print('apertou carteira');
+              },
+            ),
+            ListTile(
+              title: Text('Histórico de Corridas'),
+              trailing: Icon(Icons.equalizer),
+              onTap: () {
+                print('apertou Histórico de corridas');
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.account_circle),
+        onPressed: () {
+          /**
+         * método horrivel 
+         * 
+         */
+          _scaffoldKey.currentState.openDrawer(); //AQUII
+        },
+      ),
+      // drawerScrimColor: Colors.indigo,
     );
   }
 }
